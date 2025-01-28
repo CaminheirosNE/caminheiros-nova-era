611600f9072b973ff9024f3ebf34ced664fa3650
SBSSC <solsaobentodosul@gmail.com>
2025-01-28 10:37:55 -0300

    Update Layout.tsx

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
+import Link from 'next/link';
 
 const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   const location = useLocation();
