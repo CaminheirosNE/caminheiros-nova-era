a4b7b198fba648c29b75d2fd8c32ddcbd35cc16c
SBSSC <solsaobentodosul@gmail.com>
2025-01-29 21:04:28 -0300

    Atualizar o UserPrivilege.ts

--- src/types/UserPrivilege.ts
+++ src/types/UserPrivilege.ts
@@ -1,1 +1,19 @@
-a
+export interface UserPrivilege {
+  userId: string;
+  screenId: string;
+  level: number;
+}
+
+export interface UserPrivilegeProps {
+  privileges: UserPrivilege[];
+  userId: string;
+  screenId: string;
+  requiredLevel: number;
+}
+
+export interface PrivilegeContextData {
+  privileges: UserPrivilege[];
+  addPrivilege: (privilege: UserPrivilege) => void;
+  removePrivilege: (userId: string, screenId: string) => void;
+  updatePrivilege: (privilege: UserPrivilege) => void;
+}
